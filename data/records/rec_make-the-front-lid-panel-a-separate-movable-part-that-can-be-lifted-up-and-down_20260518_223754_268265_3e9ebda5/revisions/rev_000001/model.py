997cd4525de48e26143950270064478ec7992177
Matt Zhou <mattzh1314@berkeley.edu>
2026-05-20 14:28:35 -0400

    Restore water filter jug records PR

---
+++ data/records/rec_make-the-front-lid-panel-a-separate-movable-part-that-can-be-lifted-up-and-down_20260518_223754_268265_3e9ebda5/revisions/rev_000001/model.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8d379d7e0621276847ecc3d6c44d81f6a9f697929f91219ab4578b87baeca1d2
+size 11859
---
+++ data/records/rec_realistic-articulated-water-filter-jug_20260518_214632_682430_999fe34f/revisions/rev_000001/model.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d7da13e39555b172d575fd6876dbdaeef6ac32413632912b42b486ab51701051
+size 10311
